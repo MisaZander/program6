c73453db9af7089780627b9134f0b357f7a851a0
Alexander Osborn <axo095120@utdallas.edu>
2018-04-20 10:00:39 -0500

    On the road to figuring out the binary file contents

---
+++ testread/tester.cc
@@ -0,0 +1,68 @@
+/*
+ * Usage of CDK Matrix
+ *
+ * File:   example1.cc
+ * Author: Stephen Perkins
+ * Email:  stephen.perkins@utdallas.edu
+ */
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+//#include "cdk.h"
+#include "program6.h"
+
+
+#define MATRIX_WIDTH 3
+#define MATRIX_HEIGHT 5
+#define BOX_WIDTH 25
+#define MATRIX_NAME_STRING "Enter the Matrix"
+
+using namespace std;
+
+
+int main()
+{
+  
+  //Start with some file reading
+  ifstream infile;
+  infile.open("cs3377.bin", ios::in | ios::binary);
+  if(infile.is_open()) {
+    cout << "File open success!" << endl;
+  } else {
+    cerr << "Runtime error: Could not open cs3377.bin" << endl;
+    
+  }
+  
+  BinaryFileHeader *bfh = new BinaryFileHeader();
+  //for (int i = 0; i < 3; i++)
+    infile.read((char *) bfh, sizeof(BinaryFileHeader));
+  
+  BinaryFileRecord *bfr = new BinaryFileRecord();
+  while(infile.read((char *) bfr, sizeof(BinaryFileRecord))){
+
+    string magicWord = bfr->stringBuffer;
+    cout << "strlen: " << magicWord.length() << endl;
+    cout << "Record: " << magicWord << endl;
+  }
+
+  /*  string magicNumber;
+  ostringstream dirtyConverter;
+  dirtyConverter << "Magic: 0x" << hex << uppercase << bfh->magicNumber;
+  magicNumber = dirtyConverter.str();
+  setCDKMatrixCell(myMatrix, 1, 1, magicNumber.c_str());
+  
+  string magicWord;
+  ostringstream cleanConverter;
+  cleanConverter << "Version: " << bfh->versionNumber;
+  magicWord = cleanConverter.str();
+  setCDKMatrixCell(myMatrix, 1, 2, magicWord.c_str());
+
+  ostringstream cleanerConverter;
+  cleanerConverter << "NumRecords: " << bfh->numRecords;
+  magicWord = cleanerConverter.str();
+  setCDKMatrixCell(myMatrix, 1, 3, magicWord.c_str());
+  */
+}
